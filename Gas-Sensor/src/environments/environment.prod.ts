73c3b5da1a147be1b6537576549190cf3625015f
manhvipro123 <manhvp1810@gmail.com>
2022-06-17 10:49:38 +0700

    Haha

---
+++ Gas-Sensor/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    projectId: 'app-nct',
+    appId: '1:97233203758:web:e6f890216848dd4884e5bf',
+    databaseURL: 'https://app-nct-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'app-nct.appspot.com',
+    locationId: 'asia-southeast1',
+    apiKey: 'AIzaSyBwL7zxl482OdCjz58ztxur-vwYguOtHgw',
+    authDomain: 'app-nct.firebaseapp.com',
+    messagingSenderId: '97233203758',
+    measurementId: 'G-JV4FPQV098',
+  },
+  production: true
+};
