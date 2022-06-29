7ec59b2d39dd43f42ff410d4bb4dfdbbaa095772
manhvipro123 <manhvp1810@gmail.com>
2022-06-29 15:21:04 +0700

    History

---
+++ Gas-Sensor/src/app/models/gas.model.ts
@@ -0,0 +1,5 @@
+export interface Gas_Detail{
+    day: string,
+    gas: string,
+    time: string,
+}
--- Gas-Sensor/src/app/pages/history/history.component.ts
+++ Gas-Sensor/src/app/pages/history/history.component.ts
@@ -1,100 +1,162 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseApp } from '@angular/fire/app';
-import { getDatabase, ref, child, get } from "firebase/database";
+import { AnyForUntypedForms } from '@angular/forms';
+import { getDatabase, ref, child, get, onChildAdded } from "firebase/database";
+import { Gas_Detail } from 'src/app/models/gas.model';
+
+
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss']
 })
+
 export class HistoryComponent implements OnInit {
   basicData: any;
   basicOptions: any;
-  constructor(firebase: FirebaseApp) {
-    const db = getDatabase(firebase);
-    const myRef = ref(db,);
-    get(child(myRef, "/day")).then((snapshot) => {
-      if (snapshot.exists()) {
-        console.log(snapshot.val());
-      } else {
-        console.log("No data available");
-      }
-    }).catch((error) => {
-      console.error(error);
-    });
+  hour: any;
+  tempList: Array<Gas_Detail> = [];
+  timeList: any[] = [];
+  gasList: any[] = [];
+  
+  values : any[] = [];
+  
+  toTimestamp(dayString: string, time: string) {
+    let day = dayString.split('/');
+    let cvt_day = day[1] + "/" + day[0] + "/" + day[2];
+    let currentTimestamp = Date.parse(cvt_day + " " + time) / 1000;
+    return currentTimestamp;
   }
 
-  ngOnInit(): void {
-    this.basicOptions = {
-      plugins: {
-        legend: {
-          labels: {
-            color: '#495057'
-          }
-        }
-      },
-      scales: {
-        x: {
-          ticks: {
-            color: '#495057'
-          },
-          grid: {
-            color: '#ebedef'
-          }
-        },
-        y: {
-          ticks: {
-            color: '#495057'
-          },
-          grid: {
-            color: '#ebedef'
-          }
-        }
-      }
-    };
+  constructor(firebase: FirebaseApp) {
     this.basicData = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      labels: [],
       datasets: [
         {
-          label: 'My First dataset',
+          label: 'Gas(%)',
           backgroundColor: '#42A5F5',
-          data: [65, 59, 80, 81, 56, 55, 40]
+          data: []
         },
-        {
-          label: 'My Second dataset',
-          backgroundColor: '#FFA726',
-          data: [28, 48, 40, 19, 86, 27, 90]
-        }
       ]
     };
+    
+    const db = getDatabase(firebase);
+    const myRef = ref(db,);
+
+
+
+    get(child(myRef, "/day/23-6-2022/time")).then((snapshot) => {
+      if (snapshot.exists()) {
+        this.tempList.push(snapshot.val());
+        let convertArr = Object.values(this.tempList.flat()[0]);
+        // let current: number = 0;
+        let timeline: number = 0;
+        let total: number = 0;
+        console.log(convertArr);
+        let currentTimestamp = this.toTimestamp(convertArr[0].day, "00:00:00");
+        console.log(currentTimestamp);
+        const HOURS_OF_DAY = 24 * 60 * 60;
+        const TWO_HOURS = 2 * 60 * 60;
+        const FULLDAY_TIMESTAMP = currentTimestamp + HOURS_OF_DAY;
+        
+        console.log(FULLDAY_TIMESTAMP)
+        let currentDataIndex = 0;
+        while (currentTimestamp < FULLDAY_TIMESTAMP) {
+          console.log(currentTimestamp);
+          let totalGas = 0;
+          let totalGasCount = 0;
+          while (currentDataIndex < convertArr.length) {
+            let timestamp = this.toTimestamp(convertArr[currentDataIndex].day, convertArr[currentDataIndex].time);
+            if (currentTimestamp <= timestamp && timestamp < currentTimestamp + TWO_HOURS) {
+              totalGas += parseInt(convertArr[currentDataIndex].gas);
+              totalGasCount++;
+              currentDataIndex++;
+            }
+            else {
+              if (totalGasCount == 0) {
+                totalGasCount = 1;
+              }
+              break;
+
+            }
+          }
+          let avg_gas = totalGas / totalGasCount;
+          if (avg_gas == NaN || avg_gas == Infinity) {
+            this.values.push(0);
+          }
+          else {
+            this.values.push(Math.round(avg_gas));
+          }
+          currentTimestamp += TWO_HOURS;
+        }
+
+        console.log(this.values);
+      }
+      this.update();
+    });
+    
   }
-  // applyLightTheme() {
-  //   this.basicOptions = {
-  //       plugins: {
-  //           legend: {
-  //               labels: {
-  //                   color: '#495057'
-  //               }
-  //           }
-  //       },
-  //       scales: {
-  //           x: {
-  //               ticks: {
-  //                   color: '#495057'
-  //               },
-  //               grid: {
-  //                   color: '#ebedef'
-  //               }
-  //           },
-  //           y: {
-  //               ticks: {
-  //                   color: '#495057'
-  //               },
-  //               grid: {
-  //                   color: '#ebedef'
-  //               }
-  //           }
-  //       }
-  //   };
+
+  //   for (let i = 0; i < convertArr.length; i++) {
+
+  //     console.log(convertArr[i]);
+  //     let day = convertArr[i].day.split("/");
+  //     let cvt_day = day[1] + "/" + day[0] + "/" + day[2];
+
+  //     if(toTimestamp(cvt_day + " " + convertArr[i].time) > timeline + (2*60*60*1000)){
+  //       console.log(total);
+
+  //       timeline += 2*60*60*1000; 
+  //       const date = new Date(timeline*1000);
+  //       this.timeList.push(date.toLocaleTimeString());
+  //       console.log(this.gasList,this.timeList);
+  //     }
+  //     if (timeline <= toTimestamp(cvt_day + " " + convertArr[i].time) && timeline + (2*60*60*1000) >= toTimestamp(cvt_day + " " + convertArr[i].time)) {
+  //       total += Number(convertArr[i].gas);
+  //     }
+
+  //     // console.log(toTimestamp(cvt_day + " " + convertArr[i].time));
+  //     // console.log(toTimestamp('6/17/2022 16:18:51'));
+  //     // console.log(toTimestamp('02/13/2020 23:31:30'));
+  //   }
+  // } else {
+  //   console.log("No data available");
   // }
+  // }).catch((error) => {
+  //   console.error(error);
+  // });
+
+
+  // onChildAdded(child(myRef, "/day/17-6-2022/time"), (data) => {
+  //    this.tempList.push(data.val());
+  //    this.convert(this.tempList);
+  // });
+
+
+
+  ngOnInit(): void {  
+
+  }
+
+  update() {
+    this.basicData = {
+      labels: [2,4,6,8,10,12,14,16,18,20,22,24],
+      datasets: [
+        {
+          label: 'Gas (%)',
+          data: this.values,
+          backgroundColor: '#FFA726',
+        },
+
+      ]
+
+    }
+  }
+
 
 }
+
+
+
+
+
